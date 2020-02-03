b349cd83094e8ee6ada147fa124e772d94b5d081
SennahW <johannes.wetterlund@gmail.com>
2020-02-03 11:18:24 +0100

    Added basic timer

---
+++ 未煮熟/Assets/Scripts/Timer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI myTimeText;
+    TimeSpan myTimeLeft;
+    bool myTimerActive;
+    float myElapsedTime;
+    float myTimerLength = 120;
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTimeText.text = "Time: 00:00:00";
+        myTimerActive = true;
+    }
+
+    void StartTimer () 
+    {
+        myTimerActive = true;
+        myElapsedTime = 0f;
+    }
+
+    void StopTimer () 
+    {
+        myTimerActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myTimerActive)
+        {
+            myElapsedTime += Time.deltaTime;
+            myTimeLeft = TimeSpan.FromSeconds(myTimerLength - myElapsedTime);
+        }
+
+        if (myElapsedTime >= myTimerLength)
+        {
+            myTimerActive = false;
+        }
+
+        if (myElapsedTime == 55)
+        {
+            TimerAnimation();
+        }
+
+        string tempTimeString = "Time: " + myTimeLeft.ToString("mm' : 'ss'.'ff");
+        myTimeText.text = tempTimeString;
+    }
+
+    void TimerAnimation()
+    {
+        myTimeText.fontSize = 35;
+
+    }
+
+}
