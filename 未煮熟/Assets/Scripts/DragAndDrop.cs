84c5c9ee176fb90341c0738dcf38a9a6b97ca68c
DandyOrc <44340957+DandyOrc@users.noreply.github.com>
2020-02-03 13:18:13 +0100

    yes

---
+++ 未煮熟/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool mySelected;
+
+    void Update()
+    {
+        if(mySelected == true)
+        {
+            Vector2 myCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(myCursorPos.x, myCursorPos.y);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            mySelected = false;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mySelected = true;
+        }
+    }
+}
