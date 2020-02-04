6c7cfb8cefdd7e746f6dc7e346577dba34474dd2
DandyOrc <44340957+DandyOrc@users.noreply.github.com>
2020-02-04 10:33:27 +0100

    skrrt

---
+++ 未煮熟/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject myItemBegingDragged;
+    Vector3 myStartPosition;
+    Transform myStartParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        myItemBegingDragged = gameObject;
+        myStartPosition = transform.position;
+        myStartParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        myItemBegingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == myStartParent)
+        {
+            transform.position = myStartPosition;
+        }
+        
+    }
+}
