bc132c694c753b6cd96b965cd2dabf9848fd2c0a
DandyOrc <44340957+DandyOrc@users.noreply.github.com>
2020-02-12 12:07:53 +0100

    push woman push

---
+++ 未煮熟/Assets/Resources/Scripts/SoundManager/AudioManagerGameOver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManagerGameOver : MonoBehaviour
+{
+    public Sound[] mySounds;
+
+    void Awake()
+    {
+        foreach (Sound s in mySounds)
+        {
+            s.mySource = gameObject.AddComponent<AudioSource>();
+            s.mySource.clip = s.myClip;
+
+            s.mySource.volume = s.myVolume;
+            s.mySource.pitch = s.myPitch;
+            s.mySource.loop = s.myLoop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Game Over");
+    }
+
+    public void Play(string myName)
+    {
+        Sound s = Array.Find(mySounds, sound => sound.name == myName);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.mySource.Play();
+    }
+}
