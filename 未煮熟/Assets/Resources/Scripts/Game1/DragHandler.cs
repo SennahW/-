3d64b0d80827e43614052952dd04dc72b0fa9484
SennahW <johannes.wetterlund@gmail.com>
2020-02-13 13:54:45 +0100

    Pork fix two

--- 未煮熟/Assets/Resources/Scripts/Game1/DragHandler.cs
+++ 未煮熟/Assets/Resources/Scripts/Game1/DragHandler.cs
@@ -87,6 +87,18 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
         Destroy(this.gameObject);
     }
 
+    public void SpawnCookedPorkOne()
+    {
+        GameObject.FindGameObjectWithTag("FryingpanOne").GetComponent<Slots>().SpawnCookedPork();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnCookedPorkTwo()
+    {
+        GameObject.FindGameObjectWithTag("FryingpanTwo").GetComponent<Slots>().SpawnCookedPork();
+        Destroy(this.gameObject);
+    }
+
     public void SpawnCookedShrimpOne()
     {
         GameObject.FindGameObjectWithTag("FryingpanOne").GetComponent<Slots>().SpawnCookedShrimp();
