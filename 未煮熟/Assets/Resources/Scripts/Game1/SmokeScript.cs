4d3d45cded9116cef3690c32c2e6150b9e419e01
SennahW <johannes.wetterlund@gmail.com>
2020-02-13 09:52:01 +0100

    FIXED BURNT MÖG

---
+++ 未煮熟/Assets/Resources/Scripts/Game1/SmokeScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeScript : MonoBehaviour
+{
+    public void SpawnSmoke()
+    {
+        Debug.Log("dfjdfioh");
+        GameObject ClamCooked = Resources.Load<GameObject>("Prefabs/Smoke");
+        Instantiate<GameObject>(ClamCooked, transform);
+    }
+}
